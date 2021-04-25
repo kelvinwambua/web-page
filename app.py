3634f55093c6d56eab2be5359ec21e67ca3ac900
wisdom <wisdomwashe500@gmail.com>
2021-04-26 00:03:03 +0300

    backend to support emails and error some handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import smtplib
 
 app = Flask(__name__)
 
@@ -9,7 +9,32 @@ def home():
 
 @app.route('/more/')
 def more():
+
     return render_template('more.html')
+
+@app.route('/form', methods=['POST'])
+def form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    text = request.form.get('text')
+
+    SendmMailTO = ""
+    SenderMail = ""
+    SenderPassword = ""
+    DataToSend = name + email + text
+
+
+    message = "Your message has been sent"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(SenderMail, SenderPassword)
+    server.sendmail(SenderMail, SendmMailTO, DataToSend)
+
+    if not name or not email or not text:
+        error = "All field must be filled"
+        return render_template('error.html', error=error)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
